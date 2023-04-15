07f8701b9e438c3c779f93040c5aec3beae8a331
Mathacc <mateus.karneiro@phystech.edu>
2023-04-15 20:26:21 -0300

    dinamic

--- alura_space/galeria/models.py
+++ alura_space/galeria/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Fotografia(models.Model):
+    CATEGORY_OPTIONS = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
+    ]
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    foto= models.CharField(max_length=100, null=False, blank=False)
+    foto= models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
