9956eab8972d42b2ff32e1d34c9b404564b6f37f
Mathacc <mateus.karneiro@phystech.edu>
2023-07-01 16:50:58 -0300

    rebase

--- site-project/src/core_apps/users/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-#gettext_lazy is used as a internationalization for django
-
-class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core_apps.users"
-    verbose_name = _("Users") #create a human readable name in singular
